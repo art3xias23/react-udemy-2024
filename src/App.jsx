2f2f9a67439e93124210f6e70cc130f9401e1645
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2024-11-30 16:03:05 +0200

    Conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { CORE_CONCEPTS } from './data.js'
+import { CORE_CONCEPTS, EXAMPLES } from './data.js'
 import Header from './components/Header/Header.jsx'
 import CoreConcept from './components/CoreConcept.jsx'
 import TabButton from './components/TabButton.jsx'
@@ -7,11 +7,20 @@ import { useState } from 'react';
 
 function App() {
 
-  var [tabComponent, setTabComponent] = useState("Dynamic Content");
-
+  var [selectedTopic, setSelectedTopic] = useState();
+  var tabContent = "Please select a topic";
+  if (selectedTopic) {
+    tabContent = 
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code><p>{EXAMPLES[selectedTopic].code}</p></code>
+        </pre>
+      </div>
+  }
   function handleSelect(name) {
-    setTabComponent(name);
-    console.log(name);
+    setSelectedTopic(name);
   }
 
   console.log("Tab Component Rendering")
@@ -29,13 +38,13 @@ function App() {
           <section id="examples">
             <h2>Examples</h2>
             <menu>
-              <TabButton onSelect={() => { handleSelect("Components1") }}>Components</TabButton>
-              <TabButton onSelect={() => { handleSelect("JSX") }}>JSX</TabButton>
-              <TabButton onSelect={() => { handleSelect("Props") }}>Props</TabButton>
-              <TabButton onSelect={() => { handleSelect("State") }}>State</TabButton>
+              <TabButton onSelect={() => { handleSelect("components") }}>Components</TabButton>
+              <TabButton onSelect={() => { handleSelect("jsx") }}>JSX</TabButton>
+              <TabButton onSelect={() => { handleSelect("props") }}>Props</TabButton>
+              <TabButton onSelect={() => { handleSelect("state") }}>State</TabButton>
             </menu>
-            {tabComponent}
           </section>
+          {tabContent}
         </section>
       </main>
     </div>
