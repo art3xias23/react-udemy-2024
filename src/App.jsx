bc8b412f7e9e02ca1fadb41b5d87f5e3f14479ab
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2024-12-01 12:59:01 +0200

    further component extraction

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,18 @@
-import { CORE_CONCEPTS, EXAMPLES } from './data.js'
+import { EXAMPLES } from './data.js'
 import Header from './components/Header/Header.jsx'
-import CoreConcept from './components/CoreConcept.jsx'
-import TabButton from './components/TabButton.jsx'
-import { useState, Fragment } from 'react';
+import CoreConcepts from './components/CoreConcepts.jsx';
+import Examples from './components/Examples.jsx';
+import { Fragment } from 'react';
 
 
 function App() {
 
-  var [selectedTopic, setSelectedTopic] = useState();
-  var tabContent = "Please select a topic";
-  if (selectedTopic) {
-    tabContent = 
-      <div id="tab-content">
-        <h3>{EXAMPLES[selectedTopic].title}</h3>
-        <p>{EXAMPLES[selectedTopic].description}</p>
-        <pre>
-          <code><p>{EXAMPLES[selectedTopic].code}</p></code>
-        </pre>
-      </div>
-  }
-  function handleSelect(name) {
-    setSelectedTopic(name);
-  }
-
-  console.log("Tab Component Rendering")
   return (
     <Fragment>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title} {...conceptItem} />)}
-          </ul>
-          <section id="examples">
-            <h2>Examples</h2>
-            <menu>
-              <TabButton isSelected={selectedTopic === 'components'} onSelect={() => { handleSelect("components") }}>Components</TabButton>
-              <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => { handleSelect("jsx") }}>JSX</TabButton>
-              <TabButton isSelected={selectedTopic === 'props'} onSelect={() => { handleSelect("props") }}>Props</TabButton>
-              <TabButton isSelected={selectedTopic === 'state'} onSelect={() => { handleSelect("state") }}>State</TabButton>
-            </menu>
-          </section>
-          {tabContent}
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
     </Fragment>
   );
---
+++ src/components/Examples.jsx
@@ -0,0 +1,30 @@
+import TabButton from "./TabButton";
+import { useState } from "react";
+import { EXAMPLES } from "../data";
+import Section from "./Section.jsx"
+
+export default function Examples() {
+
+    var [selectedTopic, setSelectedTopic] = useState("components");
+    var tabContent = "Please select a topic";
+    if (selectedTopic) {
+        tabContent =
+            <div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre>
+                    <code><p>{EXAMPLES[selectedTopic].code}</p></code>
+                </pre>
+            </div>
+    }
+
+    return (
+        <Section title="Examples" id="examples">
+            <menu>
+                <TabButton isSelected={selectedTopic === 'components'} onSelect={() => { setSelectedTopic("components") }}>Components</TabButton>
+                <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => { setSelectedTopic("jsx") }}>JSX</TabButton>
+                <TabButton isSelected={selectedTopic === 'props'} onSelect={() => { setSelectedTopic("props") }}>Props</TabButton>
+                <TabButton isSelected={selectedTopic === 'state'} onSelect={() => { setSelectedTopic("state") }}>State</TabButton>
+            </menu>
+        </Section>)
+}
