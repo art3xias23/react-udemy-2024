71a00f451421ff5d59dc42377c29c8dbd2498f3a
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2024-12-02 22:08:01 +0200

    dynamic player component

---
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,11 @@
+import {useState} from 'react';
+export default function Player({name, symbol}){
+   const [isEditing, setIsEditing] =  useState(false)
+return (<li>
+            <span className="player">
+              {isEditing ? <input value={name} /> : <span className="player-name">{name}</span>}
+              <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={() => setIsEditing(!isEditing)}>Edit</button>
+          </li>)
+}
