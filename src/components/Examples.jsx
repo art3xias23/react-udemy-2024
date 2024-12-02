7b69aaec2be47899ac209e3263dc89d483b2ad10
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2024-12-02 21:26:29 +0200

    moved the gitfolder
    fsdfsd

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -22,7 +22,7 @@ export default function Examples() {
 
     return (
         <Section title="Examples" id="examples">
-            <Tabs ButtonsContainer = "menu"
+            <Tabs 
             buttons={
                 <Fragment>
                     <TabButton isSelected={selectedTopic === 'components'} onClick={() => { setSelectedTopic("components") }}>Components</TabButton>
