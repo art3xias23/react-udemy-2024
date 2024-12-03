9086ff10691b21155b56eb4d69c0e858d9bea5dc
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2024-12-03 22:01:58 +0200

    added state for playername

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,9 +1,10 @@
 import {useState} from 'react';
-export default function Player({name, symbol}){
+export default function Player({initialName, symbol}){
    const [isEditing, setIsEditing] =  useState(false)
+   const [playerName, setPlayerName] = useState(initialName);
 return (<li>
             <span className="player">
-              {isEditing ? <input value={name} /> : <span className="player-name">{name}</span>}
+              {isEditing ? <input required value={playerName} onChange={(evt) => setPlayerName(evt.target.value)} /> : <span className="player-name">{playerName}</span>}
               <span className="player-symbol">{symbol}</span>
             </span>
             <button onClick={() => setIsEditing((editing) => !editing )}>{isEditing ? "Save" : "Edit"}</button>
